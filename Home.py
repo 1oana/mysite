d5d117ae439a7746e287ce07b121e0cae0638772
Ioana Duta <ioana@Mihaelas-MacBook-Pro.local>
2025-05-08 11:28:49 +0100

    update requirements, add flake8 and add more text to homepage

--- Home.py
+++ Home.py
@@ -15,22 +15,26 @@ with tabs[0]:
     I've been working at the University of Oxford as a Data Scientist/Research Assistant.
     I've been working on projects in global and public health, as well as with education data.
     
-    What has excited me most about my work has been the variety: of data, of challenges, of people...
+    What has excited me most about my work has been the variety: of data, of challenges, of collaborators...
     I enjoy the challenge of cleaning messy data and using it to find answers (or even just more questions) .
-    I also enjoy the process of communicating my findings to others,
-    whether it's through writing reports, giving presentations, or creating visualizations.
-    I am passionate about using data to make a positive impact in the world,
-    and I believe that data science has the potential to do just that.
+    I also enjoy the process of communicating my work to a diverse audience and the opportunity to
+    collaborate with and learn from a wide range of people.
+    I am especially passionate about using the resources available to us in the global north to make 
+    a positive impact around the world.
     """)
 
     cv_button = st.expander("View a brief CV")
 
-    with st.container():
-        with cv_button:
-            st.markdown("""**Experience**
-                
-            Data Scientist/Research Assistant at University of Oxford (2022 - Present)
-            - Projects in global and public health and education data
-            - Passionate about data-driven impact
-                        
-                        """)
+    with cv_button:
+        st.markdown("""**Experience**
+
+- **Project Administrator**, January 2025-present; _Doctoral Training Centre, University of Oxford, UK_
+- **Data Scientist**, October 2022-April 2025; _Nuffield Department of Clinical Neuroscience/Oxford Martin Programme on Global Epilepsy, University of Oxford, UK_
+- **Research Assistant (AI)**, October 2023-December 2024; _Nuffield Department of Women's and Reproductive Health, University of Oxford, UK_
+                    """)
+        
+        st.markdown("""**Education**
+- **MSci in Mathematics**, 2018-2022; _Imperial College London, UK_
+    - First Class Honours (combined Bachelor's and Master's degree)
+    - Thesis: _The Spread of Information about COVID-19 on Twitter_
+""")
