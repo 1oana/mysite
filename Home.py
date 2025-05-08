423e772fcfdbaaa9439f0bc9c6cdf923fb7f08a0
Ioana Duta <ioana@Mihaelas-MacBook-Pro.local>
2025-05-08 14:20:52 +0100

    add thesis info

--- Home.py
+++ Home.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.title("Ioana's Portfolio")
 
 
-tabs = st.tabs(['Me', 'My projects'])
+tabs = st.tabs(["Me", "My projects"])
 with tabs[0]:
     st.markdown(""" 
-    My name is [Ioana](https://www.linkedin.com/in/ioana-ioana/)
+    My name is [Ioana](https://www.linkedin.com/in/ioana-duta/)
     and I made this website as a little portfolio.
     My name can be confusing to pronounce,
     so I usually tell people it 'rhymes with iguana', hence the URL.
@@ -32,9 +32,43 @@ with tabs[0]:
 - **Data Scientist**, October 2022-April 2025; _Nuffield Department of Clinical Neuroscience/Oxford Martin Programme on Global Epilepsy, University of Oxford, UK_
 - **Research Assistant (AI)**, October 2023-December 2024; _Nuffield Department of Women's and Reproductive Health, University of Oxford, UK_
                     """)
-        
+
         st.markdown("""**Education**
 - **MSci in Mathematics**, 2018-2022; _Imperial College London, UK_
     - First Class Honours (combined Bachelor's and Master's degree)
     - Thesis: _The Spread of Information about COVID-19 on Twitter_
 """)
+    with tabs[1]:
+        st.markdown("### Projects")
+
+        thesis_expander = st.expander(
+            "**Master's project: [_The Spread of Information about COVID-19 on Twitter_](https://github.com/1oana/Masters_thesis)**"
+        )
+        with thesis_expander:
+            st.markdown("""
+                _Adapted abstract_:
+                Social media acts as a forum for the exchange of ideas on a level of accessibility
+                and equalisation unprecedented in human history. Twitter in particular has become a
+                mainstay of socio-political communication. Thus, conversations on Twitter are a
+                microcosm of the spread of ideas through the population as a whole, both as a
+                mirror to information spread via other means, and as a medium of spread in and of
+                itself. The results from analysis of tweet reply networks as a conversation medium
+                reveals that it is possible to use measures of the size, depth and virality of
+                reply cascades to predict classify conversations as to whether they are or are not
+                about COVID, as well as to differentiate between accounts according to their
+                purpose and influence. Investigations of user interaction networks revealed how
+                both verified public figures and ordinary people have the power to influence
+                conversations and become central in conversational communities.
+                
+                Technique and methods:
+                - Data collection: Twitter API, web scraping
+                - Data analysis: Python
+                - Data visualisation: Gephi, PyPlot, Seaborn
+                - Machine learning: Scikit-learn
+                - Network analysis: NetworkX, Gephi
+
+                The project was supervised by [Dr. Prasun K Ray](https://profiles.imperial.ac.uk/p.ray/) and
+                submitted as part of my Master's degree in Mathematics at Imperial College London.
+
+                The code and writeup is available on my [GitHub](https://github.com/1oana/Masters_thesis).
+            """)
